94ef35526dfdce41f27831ee5e778d883f84225f
pocifik <pocific.i@gmail.com>
2018-07-19 15:05:39 +0300

    callback data

--- src/models/InlineKeyboardButton.php
+++ src/models/InlineKeyboardButton.php
@@ -8,4 +8,8 @@ class InlineKeyboardButton
      * @var string Text of the button. If none of the optional fields are used, it will be sent as a message when the button is pressed
      */
     public $text;
+    /**
+     * @var string Optional. Data to be sent in a callback query to the bot when button is pressed, 1-64 bytes
+     */
+    public $callback_data;
 }
